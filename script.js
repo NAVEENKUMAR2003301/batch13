d922e3c4abe9c974d2dc25cdc0a76690a4ad7795
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2025-08-26 11:06:18 +0530

    day 1

---
+++ script.js
@@ -0,0 +1,121 @@
+// variables
+
+//3 types
+
+// var - global scope
+// let - block scope
+// const - block scope
+
+
+//rules
+
+//var - global scope 
+
+//rule :- 
+
+var a = 21 // declration and intiazialition
+
+console.log(a);
+
+a = 10 //reuse
+
+console.log(a);
+
+var a = 12 //redeclration and reintiazialition
+
+console.log(a);
+
+
+// let - block scope
+
+let b = 10 // declration and intiazialition
+
+b = 21      // reuse // reinitiazialition
+
+// let b = 21 // not accept redeclration
+
+console.log(b);
+
+
+//const - block scope
+
+const c = 10 // declration and initialiaziation
+
+// c = 10   // reuse not accept
+
+// const c = 12 // not accept reDeclration and reIntialiaziation
+
+console.log(c);
+
+
+
+// printing Statement
+
+// console.log(1);
+
+// alert
+
+
+// alert("hello guys this our first class")
+
+
+//confirm
+
+// let a1 = confirm("did you ate breakfast")
+
+// console.log(a1); 
+
+//prompt
+
+// let userName = prompt("enter your full name")
+
+// console.log(userName);
+
+//writeLn
+
+// document.writeln("hello kjhjkhjhghh")
+
+
+
+// console.log > console method
+
+// console.log("log")
+// console.error("error")
+// console.warn("warn")
+// console.clear()
+
+
+
+// dataTypes 
+
+// 1. primitive dataTypes
+
+    //   1.Number - 23, 1000
+    //   2.string - "navi", "123"
+    //   3.boolean - true  , false 
+    //   4.null
+    //   5.undefined
+
+
+    console.log(typeof(123));
+    console.log(typeof("123"));
+    console.log(typeof(false));
+    
+    let d ;
+    
+    console.log(d);
+
+    let eName = prompt("hello")
+
+    console.log(eName);
+    
+    
+    
+
+// 2. non primitive dataTypes
+
+
+
+
+
+
